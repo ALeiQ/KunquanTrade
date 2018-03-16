bc107b4916f816a07404fb81f059acd87c2bfac1
LeiQ-PC\Administrator <422206217@qq.com>
2018-03-16 20:23:32 +0800

    常用名词折叠栏，点击增加时展开当前栏

--- src/main/resources/static/js/my-js.js
+++ src/main/resources/static/js/my-js.js
@@ -22,12 +22,3 @@ $(document).ready(function () {
         }
     }
 });
-
-// 常用名词 折叠表格 删除按钮
-$(document).ready(function () {
-    $(".term-table .closed-tool").click(function (event) {
-        $(this).parents("tr").fadeToggle(400);
-
-        return false;
-    });
-});
--- src/main/resources/static/js/my-terms-js.js
+++ src/main/resources/static/js/my-terms-js.js
@@ -2,13 +2,49 @@
  * Copyright (C) 2018 Baidu, Inc. All Rights Reserved.
  */
 
-$(document).ready(function () {
+$(function () {
 
-    $('#goodsTable').bootstrapTable()
+    $('.collapse-toggle').collapse({
+        parent: accordion
+    });
+
+    $('#goodsTable').bootstrapTable({
+        columns: [
+            {field:"id", edit:false,title:"编号"},
+            {field:"id", title:"名称", width: "150px"}
+        ]
+    });
+
+    $('#companysTable').bootstrapTable({
+
+    });
+
+    $('#transportTable').bootstrapTable({
+
+    });
+
+
+    $('#bankTable').bootstrapTable({
+
+    });
+
+    $('.add-row-button').click(function () {
+        var panel = $(this).parents('.panel');
+        panel.children('.collapse').collapse('show');
+    });
 
     $('#addGoods').click(function () {
-        var data = {};
-        $('#goodsTable').bootstrapTable('append', data);
-    })
+        //var data = {};
+        //$('#goodsTable').bootstrapTable('append', data);
+    });
     
+});
+
+// 常用名词 折叠表格 删除按钮
+$(document).ready(function () {
+    $(".term-table .closed-tool").click(function (event) {
+        $(this).parents("tr").fadeToggle(400);
+
+        return false;
+    });
 });
